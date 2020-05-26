f1630515778b652ccf7824044c2aa8a50018f2fb
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-25 22:44:33 -0500

    added nazar and typewriter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo192.png';
 import './App.css';
+import 'tachyons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header hover-white">
+        <img src={logo} className="App-logo" alt="logo" cursor="pointer"/>
+        <div className="typewriter">
+          <p className="typewriter-text">Hi! My name is Magnus!</p>     
+        </div>
+      </div>
     </div>
   );
 }
