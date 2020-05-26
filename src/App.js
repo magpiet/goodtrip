d235087fe25fbfc60768ccf52456a7e7bed0ec9d
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-26 00:31:34 -0500

    testing some changes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import 'tachyons'
 function App() {
   return (
     <div className="App">
-      <div className="App-header hover-white">
-        <img src={logo} className="App-logo" alt="logo" cursor="pointer"/>
+      <div className="App-header">
+        <a href="https://www.google.com/" className="pointer">
+          <img src={logo} className="App-logo" alt="logo"/>
+        </a>
         <div className="typewriter">
-          <p className="typewriter-text">Hi! My name is Magnus!</p>     
+          <p className="typewriter-text">Hi! My name is Magnus!</p>
         </div>
       </div>
     </div>
