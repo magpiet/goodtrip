76af435ffcb0e11af07df18ae2d22c852f35f167
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-26 22:03:17 -0500

    added music!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import logo from './logo192.png';
 import './App.css';
-import 'tachyons'
+import 'tachyons';
+import sza from './sza.mp3';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <a href="https://www.google.com/" className="pointer">
-          <img src={logo} className="App-logo" alt="logo"/>
-        </a>
-        <div className="typewriter">
-          <p className="typewriter-text">Hi! My name is Magnus!</p>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      play: true,
+    }
+    this.audio = new Audio(sza);
+  }
+
+  playAudio = () => {
+    if(this.state.play) {
+      this.setState({play: false});
+      this.audio.play();
+      console.log('play');
+    } else {
+      this.setState({play: true});
+      this.audio.pause();
+      console.log('pause');
+    }
+  }
+
+  render() {
+     return (
+        <div className="App">
+          <div className="App-header">
+            <p onClick={this.playAudio} className="pointer">
+              <img src={logo} className="App-logo" alt="logo"/>
+            </p>
+            <div className="typewriter">
+              <p className="typewriter-text">Hi! My name is Magnus!</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+     );
+  }
 }
 
 export default App;
