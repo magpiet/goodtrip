30b2224e43d24274b6ef5e412a1dcf8b4b52198b
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-29 17:50:10 -0500

    few changes for mobile testing

--- src/Cards.js
+++ src/Cards.js
@@ -3,7 +3,7 @@ import './Cards.css';
 
 const Card = ({name, about, link, image}) => {
 	return (
-		<div className='tc bg-navy pa1 dib h w br4 ma3 fade-in shadow-5'>
+		<div className='tc bg-navy pa2 dib size br4 ma3 fade-in shadow-5'>
 			<img onClick={() => window.open(link, '_blank')} src={image} alt='' className='grow pa2 h5 w5'/>
 			<div>
 				<h1 className='b white'>{name}</h1>
