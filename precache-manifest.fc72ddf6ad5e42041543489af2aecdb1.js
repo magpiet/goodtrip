e85cc7d231b4c3b35a5acfabe17bd88fdaa3e222
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-28 23:41:50 -0500

    Updates

---
+++ precache-manifest.fc72ddf6ad5e42041543489af2aecdb1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f48ef8f12150a24761de0d70db4a940",
+    "url": "/goodtrip/index.html"
+  },
+  {
+    "revision": "71bbaeb76ca0fd16ea7e",
+    "url": "/goodtrip/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eb828d86f54e63e78d02",
+    "url": "/goodtrip/static/css/main.6e08f59a.chunk.css"
+  },
+  {
+    "revision": "71bbaeb76ca0fd16ea7e",
+    "url": "/goodtrip/static/js/2.cf881633.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodtrip/static/js/2.cf881633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb828d86f54e63e78d02",
+    "url": "/goodtrip/static/js/main.738fa152.chunk.js"
+  },
+  {
+    "revision": "e15c5120db9dc23a85a7",
+    "url": "/goodtrip/static/js/runtime-main.373d67f9.js"
+  },
+  {
+    "revision": "a1274c70e28358db9eee12d84910ff5e",
+    "url": "/goodtrip/static/media/art512.a1274c70.png"
+  },
+  {
+    "revision": "0b4460dc094d5ef9021822c08ef05023",
+    "url": "/goodtrip/static/media/book512.0b4460dc.png"
+  },
+  {
+    "revision": "1feb2a289a4a665c8f011d59224a7cc2",
+    "url": "/goodtrip/static/media/heart512.1feb2a28.png"
+  },
+  {
+    "revision": "cfc2d9d087d1ef49b1907b00020af29f",
+    "url": "/goodtrip/static/media/logo512.cfc2d9d0.png"
+  },
+  {
+    "revision": "f2272a8ca33434c2e15786028fab46f6",
+    "url": "/goodtrip/static/media/music512.f2272a8c.png"
+  },
+  {
+    "revision": "b86ce54093509cbca8f76c4de1e725bc",
+    "url": "/goodtrip/static/media/planet512.b86ce540.png"
+  },
+  {
+    "revision": "833d18e6bc0b53dd4e0f781fd8fbdd11",
+    "url": "/goodtrip/static/media/space512.833d18e6.png"
+  },
+  {
+    "revision": "64f7f6c638b2c4bc226d39a0429e5f48",
+    "url": "/goodtrip/static/media/sza.64f7f6c6.mp3"
+  }
+]);
