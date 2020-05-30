b0a1eda8b40fb3cc5122c076b283071f97c0fc9d
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-29 23:56:26 -0500

    cleaned up app and fixed a few broken links

---
+++ src/components/Cards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Cards.css';
+
+const Card = ({name, about, link, image}) => {
+	return (
+		<div className='tc bg-navy pa2 dib size br4 ma3 fade-in shadow-5'>
+			<img onClick={() => window.open(link, '_blank')} src={image} alt='' className='grow pa2 h5 w5'/>
+			<div>
+				<h1 className='b white'>{name}</h1>
+				<p className='b white'>{about}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
---
+++ src/components/Menu.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Cards from './Cards';
+import './Menu.css'
+
+class Menu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	onReturnHome = () => {
+		this.props.onRouteChange('home');
+	}
+
+	render() {
+		const {options} = this.props;
+		return(
+			<div className='body pad'>
+				{
+					options.map((option, i) => {
+						return (
+							<Cards 
+							name={options[i].name}
+							about={options[i].about}
+							link={options[i].link} 
+							image={options[i].image}
+							/>
+						);
+					})
+				}
+			</div>
+		);
+	}
+}
+
+export default Menu;
