0ca03b9b3ae0be1bdc6ddfcc66bedab51d424651
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-30 14:37:36 -0500

    added new options!

--- src/components/options.js
+++ src/components/options.js
@@ -4,48 +4,56 @@ import art from './art512.png';
 import book from './book512.png';
 import space from './space512.png';
 import heart from './heart512.png';
+import sun from './sun512.png';
+import jungle from './jungle512.png';
 
 export const options = [
 {
-	id: 1,
 	image: heart,
 	name: 'Self Care',
 	about: 'Feeling a little down? Take some time to analyze how you are feeling, practice mindfulness, or meditate!',
 	link: 'https://www.nytimes.com/guides/well/how-to-meditate'
 },
 {
-	id: 2,
 	image: music,
 	name: 'Lo-Fi Beats',
 	about: 'Feeling a little stressed? Dive into some music with a hypnotic bass and velvet melodies!',
 	link: 'https://www.youtube.com/watch?v=-5KAN9_CzSA'
 },
 {
-	id: 3,
 	image: planet,
 	name: 'Google Earth',
 	about: 'Feeling a little stuck? Why not take a trip around the globe and learn more about the planet!',
 	link: 'https://earth.google.com/web/'
 },
 {
-	id: 4,
-	image: art,
-	name: 'Art Palette',
-	about: 'Feeling a little creative? Take your imagination and this palette to paint whatever you feel like creating!',
-	link: 'https://sumo.app/paint/en'
+	image: sun,
+	name: 'Sunset',
+	about: 'Feeling a little tranquil? Watch the sky from a beautiful beach as the sun sets into a peaceful and ebony night!',
+	link: 'https://www.youtube.com/watch?v=jEnd8JIMii4'
 },
 {
-	id: 5,
+	image: jungle,
+	name: 'Jungle',
+	about: 'Feeling a little wild? Take an acoustically and visually stunning journey through the alluring jungle!',
+	link: 'https://www.youtube.com/watch?v=yVOw5fsUCfQ'
+},
+{
 	image: book,
 	name: 'Wikipedia',
 	about: 'Feeling a little curious? Explore the enormous library of Wikipedia and learn about the world around you!',
 	link: 'https://en.wikipedia.org/wiki/Main_Page'
 },
 {
-	id: 6,
 	image: space,
 	name: 'Alan Watts',
 	about: 'Feeling a little dreamy? Here are some words from Alan Watts, a powerful writer and explorer of consciousness!',
 	link: 'https://www.youtube.com/watch?v=h7W6fYbmF8U&list=PLDrE8eovyz8DQHrs2BXdVIYgRdRMVsjOb&index=1'
+},
+{
+	image: art,
+	name: 'Art Palette',
+	about: 'Feeling a little creative? Take your imagination and this palette to paint whatever you feel like creating!',
+	link: 'https://sumo.app/paint/en'
 },
 ]
